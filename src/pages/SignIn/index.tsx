781018de43c1f68496e91edf5f350b3b2a98dee3
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-31 12:55:11 -0300

    Páginas de SignIn e SignUp

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <h1>Esta será a página de Login</h1>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <h1>Esta será a página de cadastro</h1>
+    </>
+  );
+};
+
+export default SignUp;
