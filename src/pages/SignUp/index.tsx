feaf7f89e2675b6cfb4b40bd7ad022c252337792
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-31 18:19:37 -0300

    Página de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Background, CardContainer, TextContainer } from './styles';
+
+import { FiUser, FiMail, FiLock, FiSmartphone, FiLogOut } from 'react-icons/fi';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
   return (
-    <>
-      <h1>Esta será a página de cadastro</h1>
-    </>
+    <Container>
+      <CardContainer>
+        <TextContainer>
+          <h1>
+            Comparti<span>ler</span>
+          </h1>
+          <p>
+            Ajude pessoas a adquirirem conhecimento através dos livros.<br></br>
+            <span>Saiba mais.</span>
+          </p>
+        </TextContainer>
+        <form action="">
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="whatsapp"
+            icon={FiSmartphone}
+            placeholder="Whatsapp (opcional)"
+          />
+          <Input
+            type="password"
+            icon={FiLock}
+            name="password"
+            placeholder="Senha"
+          />
+          <Button>Cadastre-se</Button>
+        </form>
+
+        <Link to="/">
+          <FiLogOut size={20} />
+          Voltar
+        </Link>
+      </CardContainer>
+      <Background />
+    </Container>
   );
 };
 
---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+import signInBackgroundImage from '../../assets/friends_online.svg';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 500px;
+
+  background-color: #fff;
+
+  border: 4px solid #e6e6e6;
+  border-radius: 16px;
+
+  margin: 32px 0px 32px 32px;
+  padding: 16px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    width: 90%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    text-decoration: none;
+
+    > svg {
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    font-size: 40px;
+    font-family: 'Merienda One', cursive;
+
+    span {
+      font-family: 'Merienda One', cursive;
+      color: #ff3c38;
+    }
+  }
+  p {
+    font-size: 18px;
+    margin-top: 2px;
+
+    span {
+      cursor: pointer;
+      font-size: 12px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImage}) no-repeat center;
+  background-size: cover;
+`;
+
+const OtherColors = styled.div`
+  /* background-color: #8093f1;
+  background-color: #ea7317;
+  background-color: #ff3c38; */
+`;
