f23af2397062772709a273be10e2eaa3d50fd8bf
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-11 11:11:43 -0300

    Providers de contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes';
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AppProvider>
       <Routes />
-    </BrowserRouter>
+    </AppProvider>
+
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
