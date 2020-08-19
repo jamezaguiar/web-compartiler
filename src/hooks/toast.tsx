67d8d45ed7cc8d7749da297f46c341e2addda48b
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-19 19:48:57 -0300

    ESLint fix

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -31,13 +31,13 @@ export const ToastProvider: React.FC = ({ children }) => {
         description,
       };
 
-      setMessages((state) => [...state, toast]);
+      setMessages(state => [...state, toast]);
     },
     [],
   );
 
   const removeToast = useCallback((id: string) => {
-    setMessages((state) => state.filter((message) => message.id !== id));
+    setMessages(state => state.filter(message => message.id !== id));
   }, []);
 
   return (
