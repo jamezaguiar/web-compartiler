b73b9bf8b972fdd7ac8cd109005621c06df803f8
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-20 19:22:04 -0300

    Página de registro de livros criada

---
+++ src/pages/NewBook/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewBook: React.FC = () => {
+  return <h1>New Book Page</h1>;
+};
+
+export default NewBook;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import NewBook from '../pages/NewBook';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
     <Route path="/cadastrar" component={SignUp} />
 
     <Route path="/inicio" component={Dashboard} isPrivate />
+    <Route path="/novoLivro" component={NewBook} isPrivate />
   </Switch>
 );
 
