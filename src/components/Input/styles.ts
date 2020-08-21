421fe04164f6ced56280bc3c224f59f837b4e389
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-21 19:10:11 -0300

    Correção de importação do Tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-import Tooltip from '../../components/Tooltip';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isErrored: boolean;
