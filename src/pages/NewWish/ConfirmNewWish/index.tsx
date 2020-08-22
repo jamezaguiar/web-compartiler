4c8785e1e20bd619453098e995957a6195e3e0e8
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-22 15:00:32 -0300

    Página de confirmação de novo desejo finalizada

--- src/pages/NewWish/ConfirmNewWish/index.tsx
+++ src/pages/NewWish/ConfirmNewWish/index.tsx
@@ -1,19 +1,136 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Link, useRouteMatch, useHistory } from 'react-router-dom';
+
+import { FiArrowLeft } from 'react-icons/fi';
+import { useToast } from '../../../hooks/toast';
 
 import Header from '../../../components/Header';
+import Button from '../../../components/Button';
+import { BookContainer, BookInformation, SynopsisText } from './styles';
 
-interface BookParams {
+import api from '../../../services/api';
+
+interface NewWishParams {
   isbn: string;
 }
 
+interface Book {
+  isbn: string;
+  title: string;
+  author: string;
+  cover_url: string;
+  synopsis: string;
+}
+
+interface Wish {}
+
+interface RegisterWishResponseDTO {
+  wish: Wish;
+  book: Book;
+}
+
+interface BookStatusDTO {
+  success: boolean;
+  code: number;
+  message?: string;
+}
+
+interface BookDataDTO {
+  isbn: string;
+  titulo: string;
+  contribuicao: [
+    {
+      nome: string;
+      sobrenome: string;
+      tipo_de_contribuicao: string;
+      codigo_contribuicao: string;
+    },
+  ];
+  sinopse: string;
+  imagens: {
+    imagem_primeira_capa: {
+      pequena: string;
+      media: string;
+      grande: string;
+    };
+  };
+}
+
+interface APIResponseDTO {
+  books: BookDataDTO[];
+  status: BookStatusDTO;
+}
+
 const ConfirmNewWish: React.FC = () => {
-  const { params } = useRouteMatch<BookParams>();
+  const { params } = useRouteMatch<NewWishParams>();
+
+  const [book, setBook] = useState<BookDataDTO>({} as BookDataDTO);
+  const [searchDone, setSearchDone] = useState(false);
+
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  useEffect(() => {
+    api
+      .get<APIResponseDTO>(`/externalAPI/searchBookByISBN?isbn=${params.isbn}`)
+      .then(response => {
+        setBook(response.data.books[0]);
+        setSearchDone(true);
+      });
+  }, [params.isbn]);
+
+  const handleAddBookAsANewWish = useCallback(async () => {
+    try {
+      const response = await api.post<RegisterWishResponseDTO>(
+        '/wishes/register',
+        {
+          isbn: params.isbn,
+        },
+      );
+
+      addToast({
+        type: 'success',
+        title: 'Livro registrado com sucesso',
+        description: `O livro "${response.data.book.title}" foi adicionado a sua lista de desejos!`,
+      });
+
+      history.push('/inicio');
+    } catch (err) {
+      addToast({
+        type: 'error',
+        title: 'Erro ao registrar livro',
+        description:
+          'Não foi possível adicionar livro a sua lista de desejos, tente novamente.',
+      });
+    }
+  }, [addToast, history, params.isbn]);
 
   return (
     <>
       <Header />
-      <h1>{params.isbn}</h1>
+      {searchDone && (
+        <BookContainer>
+          <img
+            src="https://i0.wp.com/www.guiada3aidade.com.br/wp-content/uploads/2018/10/sem-capa.jpg"
+            alt="Capa do livro"
+          />
+          <BookInformation>
+            <div>
+              <h1>{book.titulo}</h1>
+              <Link to="/novoDesejo">
+                <FiArrowLeft size={20} /> Voltar
+              </Link>
+            </div>
+            <p>{`de ${book.contribuicao[0].nome} ${book.contribuicao[0].sobrenome}`}</p>
+            <SynopsisText>
+              <strong>Sinopse: </strong>
+              <br />
+              {book.sinopse}
+            </SynopsisText>
+            <Button onClick={handleAddBookAsANewWish}>Adicionar</Button>
+          </BookInformation>
+        </BookContainer>
+      )}
     </>
   );
 };
---
+++ src/pages/NewWish/ConfirmNewWish/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const BookContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100vw;
+  height: 92vh;
+
+  padding: 16px;
+
+  img {
+    height: 80vh;
+  }
+`;
+
+export const BookInformation = styled.div`
+  max-width: 70vw;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    a {
+      display: flex;
+      align-items: center;
+
+      text-decoration: none;
+    }
+    h1 {
+      font-size: 64px;
+    }
+    svg {
+      margin-right: 8px;
+    }
+  }
+
+  button {
+    margin-top: 32px;
+  }
+`;
+
+export const SynopsisText = styled.p`
+  text-align: justify;
+
+  margin-top: 32px;
+`;
