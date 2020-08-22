223306fe926460e4deb27f5b6d80b7bc89500ae1
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-22 15:08:05 -0300

    Tratamento de erros e uso do unform

--- src/pages/NewWish/index.tsx
+++ src/pages/NewWish/index.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+
 import { FiSearch } from 'react-icons/fi';
+
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { useToast } from '../../hooks/toast';
+
 import api from '../../services/api';
 
 import Header from '../../components/Header';
+import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import {
-  SearchContainer,
-  InputContainer,
-  BooksContainer,
-  Book,
-  SynopsisText,
-} from './styles';
+import { SearchContainer, BooksContainer, Book, SynopsisText } from './styles';
 
 interface BookStatusDTO {
   success: boolean;
@@ -45,57 +51,107 @@ interface APIResponseDTO {
   status: BookStatusDTO;
 }
 
+interface NewWishFormData {
+  title: string;
+}
+
 const NewWish: React.FC = () => {
-  const [searchTitle, setSearchTitle] = useState('');
+  const formRef = useRef<FormHandles>(null);
+
+  const history = useHistory();
+
+  const { addToast } = useToast();
+
   const [fetchedBooks, setFetchedBooks] = useState<APIResponseDTO>(
     {} as APIResponseDTO,
   );
   const [searchDone, setSearchDone] = useState(false);
 
+  const handleSubmit = useCallback(
+    async (data: NewWishFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          title: Yup.string().required(
+            'Digite o titulo do livro que quer buscar',
+          ),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const response = await api.get<APIResponseDTO>(
+          `/externalAPI/searchBooksByTitle?title=${data.title}`,
+        );
+
+        if (!response.data.status.success) {
+          addToast({
+            type: 'error',
+            title: 'Livro não encontrado',
+            description:
+              'Não foram encontrados livros com esse titulo, tente novamente.',
+          });
+
+          return;
+        }
+
+        setFetchedBooks(response.data);
+        setSearchDone(true);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na busca',
+          description:
+            'Ocorreu um erro ao buscar livro, verifique o titulo e tente novamente',
+        });
+      }
+    },
+    [addToast],
+  );
+
   return (
     <>
       <Header />
       <SearchContainer>
-        <InputContainer>
-          <FiSearch size={20} />
-          <input
-            type="text"
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input
+            name="title"
             placeholder="Digite o titulo do livro que você quer buscar"
-            value={searchTitle}
-            onChange={event => {
-              setSearchTitle(event.target.value);
-            }}
+            icon={FiSearch}
           />
-        </InputContainer>
-        <Button
-          type="submit"
-          onClick={async () => {
-            const response = await api.get(
-              `/externalAPI/searchBooksByTitle?title=${searchTitle}`,
-            );
-
-            setFetchedBooks(response.data);
-            setSearchDone(true);
-          }}
-        >
-          Buscar
-        </Button>
+          <Button type="submit">Buscar</Button>
+        </Form>
       </SearchContainer>
       <BooksContainer>
         {searchDone &&
           fetchedBooks.books.map(book => (
             <Book key={book.isbn}>
-              <img
-                src={book.imagens.imagem_primeira_capa.grande}
-                alt="Capa do livro"
-              />
-              <h1>{book.titulo}</h1>
-              <p>{`${book.contribuicao[0].nome} ${book.contribuicao[0].sobrenome}`}</p>
-              <SynopsisText>
-                <strong>Sinopse: </strong>
-                <br />
-                {book.sinopse}
-              </SynopsisText>
+              <Link to={`/confirmarNovoDesejo/${book.isbn}`}>
+                <img
+                  src={
+                    book.imagens.imagem_primeira_capa &&
+                    book.imagens.imagem_primeira_capa.grande
+                  }
+                  alt="Capa do livro"
+                />
+                <h1>{book.titulo}</h1>
+                <p>{`${book.contribuicao[0].nome} ${book.contribuicao[0].sobrenome}`}</p>
+                <SynopsisText>
+                  <strong>Sinopse: </strong>
+                  <br />
+                  {book.sinopse}
+                </SynopsisText>
+              </Link>
             </Book>
           ))}
       </BooksContainer>
--- src/pages/NewWish/styles.ts
+++ src/pages/NewWish/styles.ts
@@ -3,51 +3,25 @@ import { shade } from 'polished';
 
 export const SearchContainer = styled.div`
   display: flex;
+
   align-items: center;
 
   margin: 16px 16px 0 16px;
 
-  button {
-    width: 150px;
-    height: 58px;
-    margin-top: 0;
-  }
-`;
+  form {
+    display: flex;
 
-export const InputContainer = styled.div`
-  display: flex;
-  align-items: center;
+    width: 100%;
 
-  width: 100%;
-
-  padding: 16px;
-  margin-right: 16px;
-
-  background: ${shade(0.075, '#e8d6cb')};
-
-  color: #12130f;
-
-  border-radius: 8px;
-  border: 2px solid #12130f;
-
-  input {
-    flex: 1;
-
-    background: transparent;
-
-    border: 0;
-
-    color: #12130f;
-
-    font-size: 16px;
-
-    &::placeholder {
-      color: #666360;
+    input {
+      flex: 1;
     }
   }
 
-  svg {
-    margin-right: 16px;
+  button {
+    width: 150px;
+    height: 58px;
+    margin: 0 0 0 8px;
   }
 `;
 
@@ -65,26 +39,41 @@ export const Book = styled.div`
   flex-direction: column;
   align-items: center;
 
-  border: 2px solid #666360;
-  min-width: 25vw;
-  max-width: 25vw;
-  height: 70vh;
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
 
-  margin-right: 8px;
+    h1 {
+      text-align: center;
+      margin: 8px 0;
+    }
 
-  background-color: ${shade(0.075, '#e8d6cb')};
-  border-radius: 4px;
+    border: 2px solid #666360;
+    min-width: 350px;
+    width: auto;
+    height: 70vh;
 
-  img {
-    margin: 8px 0;
-    width: 128px;
+    margin-right: 8px;
+
+    background-color: ${shade(0.075, '#e8d6cb')};
+    border-radius: 4px;
+
+    img {
+      margin: 8px 0;
+      width: 128px;
+    }
   }
 `;
 
 export const SynopsisText = styled.p`
-  overflow: hidden;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
 
-  text-overflow: ellipsis;
-
-  margin: 4px;
+  margin: 8px;
 `;
