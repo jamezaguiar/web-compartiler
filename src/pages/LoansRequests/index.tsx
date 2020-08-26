a3dc9c713b6eddac0f8a77064c204d77377b5304
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-25 21:44:53 -0300

    Listando solicitações de empréstimos e aceitando/rejeitando

--- src/pages/LoansRequests/index.tsx
+++ src/pages/LoansRequests/index.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 
+import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
 import api from '../../services/api';
 
+import { useToast } from '../../hooks/toast';
+
 import Header from '../../components/Header';
+import Button from '../../components/Button';
+
+import { PageTitle, Container, LoansTable } from './styles';
 
 interface LoansRequestsParams {
   user_id: string;
@@ -18,6 +24,13 @@ interface Book {
   cover_url: string;
 }
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  whatsapp?: string;
+}
+
 interface Loan {
   id: string;
   requester_id: string;
@@ -28,29 +41,122 @@ interface Loan {
   received_at: Date;
   returned_at: Date;
   book: Book;
+  book_owner: User;
+  requester: User;
 }
 
 const LoansRequests: React.FC = () => {
   const { params } = useRouteMatch<LoansRequestsParams>();
+  const history = useHistory();
 
   const [loans, setLoans] = useState<Loan[]>([{} as Loan]);
   const [searchDone, setSearchDone] = useState(false);
+
+  const { addToast } = useToast();
 
   useEffect(() => {
     api
       .get<Loan[]>(`/loans/requestedLoans/${params.user_id}`)
       .then(response => {
-        console.log(response.data);
         setLoans(response.data);
         setSearchDone(true);
       });
   }, [params.user_id]);
 
+  const handleAcceptLoan = useCallback(
+    async loan_id => {
+      await api.put(`/loans/acceptLoan/${loan_id}`);
+
+      api
+        .get<Loan[]>(`/loans/requestedLoans/${params.user_id}`)
+        .then(response => {
+          setLoans(response.data);
+          setSearchDone(true);
+        });
+
+      addToast({
+        type: 'success',
+        title: 'Sucesso!',
+        description: 'Redirecionar usuário para página de contato.',
+      });
+    },
+    [addToast, params.user_id],
+  );
+
+  const handleRejectLoan = useCallback(
+    async loan_id => {
+      try {
+        await api.put(`/loans/rejectLoan/${loan_id}`);
+
+        api
+          .get<Loan[]>(`/loans/requestedLoans/${params.user_id}`)
+          .then(response => {
+            setLoans(response.data);
+            setSearchDone(true);
+          });
+
+        addToast({
+          type: 'info',
+          title: 'Rejeitado',
+          description: 'Empréstimo rejeitado.',
+        });
+      } catch (err) {
+        addToast({
+          type: 'error',
+          title: 'Erro',
+          description: 'Não foi possível rejeitar empréstimo, tente novamente.',
+        });
+      }
+    },
+    [addToast, params.user_id],
+  );
+
   return (
     <>
       <Header />
-      {searchDone &&
-        loans.map(loan => <h1 key={loan.id}>{loan.book.title}</h1>)}
+      <PageTitle>
+        Solicitações de <span>empréstimos</span>
+      </PageTitle>
+      <Container>
+        <LoansTable>
+          <tr>
+            <th>Capa</th>
+            <th>Titulo</th>
+            <th>Pedido por:</th>
+            <th>Resposta</th>
+          </tr>
+          {searchDone &&
+            loans
+              .filter(loan => loan.status === 'requested')
+              .map(loan => (
+                <tr key={loan.id}>
+                  <td>
+                    <img src={loan.book.cover_url} alt="Capa do livro" />
+                  </td>
+                  <td>
+                    <strong>{loan.book.title}</strong>
+                  </td>
+                  <td>{loan.requester.name}</td>
+                  <td>
+                    <Button
+                      onClick={() => {
+                        handleAcceptLoan(loan.id);
+                      }}
+                    >
+                      <FiCheckCircle size={24} style={{ stroke: '#3CB371' }} />
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        handleRejectLoan(loan.id);
+                      }}
+                    >
+                      <FiXCircle size={24} style={{ stroke: '#DC143C' }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+        </LoansTable>
+      </Container>
     </>
   );
 };
---
+++ src/pages/LoansRequests/styles.ts
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const PageTitle = styled.h1`
+  font-family: 'Merienda One', cursive;
+  text-align: center;
+
+  margin: 32px 0;
+
+  span {
+    font-family: 'Merienda One', cursive;
+    color: #ff3c38;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoansTable = styled.table`
+  width: 100%;
+
+  border-collapse: collapse;
+
+  tr {
+    background-color: #e6e6e6;
+    height: 36px;
+
+    th {
+      border: 1px solid #ddd;
+      background: #ea7317;
+      color: #e6e6e6;
+    }
+
+    td {
+      border: 1px solid #ddd;
+      text-align: center;
+
+      button {
+        width: auto;
+        padding: 16px;
+
+        margin: 0;
+
+        background: transparent;
+
+        &:hover {
+          background-color: transparent;
+        }
+
+        svg {
+          stroke: #12130f;
+        }
+
+        & + button {
+          margin-left: 8px;
+        }
+      }
+    }
+  }
+  tr:nth-child(even) {
+    background-color: ${shade(0.075, '#e6e6e6')};
+  }
+`;
