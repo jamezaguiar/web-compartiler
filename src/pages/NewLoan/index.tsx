49368f41e42069d508cf0d2b655bbaf721ef6d6c
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-27 18:42:05 -0300

    Listagem de livros disponíveis para empréstimos

--- src/pages/NewLoan/index.tsx
+++ src/pages/NewLoan/index.tsx
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { FiBookOpen } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
 
 import Header from '../../components/Header';
+import Button from '../../components/Button';
 
-import { PageTitle } from './styles';
+import { PageTitle, Container, BooksTable } from './styles';
+
+import api from '../../services/api';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+}
+
+interface Book {
+  id: string;
+  isbn: string;
+  title: string;
+  author: string;
+  synopsis: string;
+  cover_url: string;
+  owner: User;
+}
 
 const NewLoan: React.FC = () => {
+  const [availableBooks, setAvailableBooks] = useState<Book[]>([{} as Book]);
+  const [searchDone, setSearchDone] = useState(false);
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    api.get<Book[]>(`/books/listAvailable/${user.id}`).then(response => {
+      setAvailableBooks(response.data);
+      setSearchDone(true);
+    });
+  }, [user.id]);
+
+  const handleRequestLoan = useCallback(() => {
+    console.log('handleRequestLoan');
+  }, []);
+
   return (
     <>
       <Header />
       <PageTitle>
         Novo <span>empréstimo</span>
       </PageTitle>
+      <Container>
+        <BooksTable>
+          <tr>
+            <th>Capa</th>
+            <th>Titulo</th>
+            <th>Dono</th>
+            <th>Solicitar</th>
+          </tr>
+
+          {searchDone &&
+            availableBooks.map(book => (
+              <tr>
+                <td>
+                  <img src={book.cover_url} alt="Capa do livro" />
+                </td>
+                <td>
+                  <strong>{book.title}</strong>
+                </td>
+                <td>{book.owner.name}</td>
+                <td>
+                  <Button>Solicitar</Button>
+                </td>
+              </tr>
+            ))}
+        </BooksTable>
+      </Container>
     </>
   );
 };
--- src/pages/NewLoan/styles.ts
+++ src/pages/NewLoan/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const PageTitle = styled.h1`
   font-family: 'Merienda One', cursive;
@@ -9,5 +10,56 @@ export const PageTitle = styled.h1`
   span {
     font-family: 'Merienda One', cursive;
     color: #ff3c38;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BooksTable = styled.table`
+  width: 100%;
+
+  border-collapse: collapse;
+
+  tr {
+    background-color: #e6e6e6;
+    height: 36px;
+
+    th {
+      border: 1px solid #ddd;
+      background: #ea7317;
+      color: #e6e6e6;
+    }
+
+    td {
+      border: 1px solid #ddd;
+      text-align: center;
+
+      padding: 16px;
+
+      img {
+        width: 128px;
+        margin: 4px;
+      }
+
+      button {
+        width: auto;
+        padding: 16px;
+
+        margin: 0;
+
+        svg {
+          stroke: #e6e6e6;
+          margin-right: 4px;
+        }
+      }
+    }
+  }
+  tr:nth-child(even) {
+    background-color: ${shade(0.075, '#e6e6e6')};
   }
 `;
